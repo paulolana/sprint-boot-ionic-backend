c9ef00c20b27afef0ec0ca29b7882bfbaf8a363a
Paulo Lana <pjlana@gmail.com>
2020-06-30 13:42:46 -0300

    Projeto criado

---
+++ src/test/java/com/paulolana/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulolana.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
