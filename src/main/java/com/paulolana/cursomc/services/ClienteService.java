3959b14aec8ac135031f823280981cc079396c9d
Paulo Lana <pjlana@gmail.com>
2020-07-01 16:47:53 -0300

    Atualizando Categoria com PUT

--- src/main/java/com/paulolana/cursomc/services/ClienteService.java
+++ src/main/java/com/paulolana/cursomc/services/ClienteService.java
@@ -15,7 +15,7 @@ public class ClienteService {
 	@Autowired // Será automaticamente instanciada pelo spring
 	private ClienteRepository repo;
 	
-	public Cliente buscar(Integer id) {
+	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Cliente não localizado. ID = " + id + ", Tipo = " + Cliente.class.getName()));
 		
