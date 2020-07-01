19cdc1e416f1c33d45cea57829fb5653f400401b
Paulo Lana <pjlana@gmail.com>
2020-07-01 10:21:37 -0300

    Cliente, Tipo Cliente, Telefones e Endereços

--- src/main/java/com/paulolana/cursomc/CursomcApplication.java
+++ src/main/java/com/paulolana/cursomc/CursomcApplication.java
@@ -10,10 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.paulolana.cursomc.domain.Categoria;
 import com.paulolana.cursomc.domain.Cidade;
+import com.paulolana.cursomc.domain.Cliente;
+import com.paulolana.cursomc.domain.Endereco;
 import com.paulolana.cursomc.domain.Estado;
 import com.paulolana.cursomc.domain.Produto;
+import com.paulolana.cursomc.domain.enums.TipoCliente;
 import com.paulolana.cursomc.repositories.CategoriaRepository;
 import com.paulolana.cursomc.repositories.CidadeRepository;
+import com.paulolana.cursomc.repositories.ClienteRepository;
+import com.paulolana.cursomc.repositories.EnderecoRepository;
 import com.paulolana.cursomc.repositories.EstadoRepository;
 import com.paulolana.cursomc.repositories.ProdutoRepository;
 
@@ -28,6 +33,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private CidadeRepository cidadeRepository;
 	@Autowired
 	private EstadoRepository estadoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired 
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -60,17 +69,27 @@ public class CursomcApplication implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c1);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 		
 		List<Categoria> categorias = Arrays.asList(cat1, cat2);
 		List<Produto> produtos = Arrays.asList(p1, p2, p3);
 		List<Estado> estados = Arrays.asList(est1, est2);
 		List<Cidade> cidades = Arrays.asList(c1, c2, c3);
+		List<Endereco> enderecos = Arrays.asList(e1, e2);
 		
 		
 		categoriaRepository.saveAll(categorias);
 		produtoRepository.saveAll(produtos);
 		estadoRepository.saveAll(estados);
 		cidadeRepository.saveAll(cidades);
+		clienteRepository.save(cli1);
+		enderecoRepository.saveAll(enderecos);
 		
 		
 	}
---
+++ src/main/java/com/paulolana/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.paulolana.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.paulolana.cursomc.domain.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
