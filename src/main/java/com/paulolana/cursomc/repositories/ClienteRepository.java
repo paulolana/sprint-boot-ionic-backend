e2f95c0394a962b32e7a18e2264c0d75a247454c
Paulo Lana <pjlana@gmail.com>
2020-07-02 16:59:28 -0300

    Validação customizada: email não repetido na inserção de cliente

--- src/main/java/com/paulolana/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/paulolana/cursomc/repositories/ClienteRepository.java
@@ -1,11 +1,17 @@
 package com.paulolana.cursomc.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.paulolana.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	//findBy<nome do campo> faz com que o Spring gere uma consulta pelo campo automaticamente
+	@Transactional(readOnly=true) 
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/paulolana/cursomc/services/validation/ClienteInsertValidador.java
+++ src/main/java/com/paulolana/cursomc/services/validation/ClienteInsertValidador.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.paulolana.cursomc.domain.enums.TipoCliente;
 import com.paulolana.cursomc.dto.ClienteNewDTO;
+import com.paulolana.cursomc.repositories.ClienteRepository;
 import com.paulolana.cursomc.resources.exceptions.FieldMessage;
 import com.paulolana.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidador implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +33,9 @@ public class ClienteInsertValidador implements ConstraintValidator<ClienteInsert
 		}
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+		if (repo.findByEmail(objDto.getEmail()) != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		// O for abaixo irá incluir a lista de FieldMessage e passará para o Framework
