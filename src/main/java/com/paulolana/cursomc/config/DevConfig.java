f7e835488be60f9f8343aed77c9b2c99a1d7736f
Paulo Lana <pjlana@gmail.com>
2020-07-03 15:40:37 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/paulolana/cursomc/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.paulolana.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.paulolana.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev") // Definido dentro de application.properties
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if (strategy.equals("create")) {
+			dbService.instantiateTestDatabase();
+				
+		}
+		
+		return true;
+	}
+}
